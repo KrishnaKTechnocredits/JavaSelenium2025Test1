fef01630d284b5dfe37238c0760fad6e6f77eef1
Krishna Kanani <kananikrishna427@gmail.com>
2025-07-20 12:35:46 +0530

    Brnach changes

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -3,7 +3,8 @@ package p1;
 public class Class1 {
 	
 	public static void main(String[] args) {
-		System.out.println("P1 c1 world");
+		System.out.println("P1 c1 world.........");
+		System.out.println("Brnach AAA changes ");
 	}
 
 }
